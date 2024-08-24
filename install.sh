e256c403c6e39dcfa422f9501d6b821b7de0f2e1
Nico Fuchs <fuchs19635@hs-ansbach.de>
2024-08-24 18:05:08 +0200

    startet using dearpygui for app

--- install.sh
+++ install.sh
@@ -5,5 +5,9 @@ python3.10 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install --upgrade pip
 
+# install pip-tools and compile dependencies
+python3 -m pip install pip-tools
+pip-compile
+
 # Install requirements
 python3 -m pip install -r requirements.txt
